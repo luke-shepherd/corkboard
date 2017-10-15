8973a64cf9ec8634be4af70ccb3d343814d49b4c
Luke <lukeshep@gmail.com>
2017-10-15 21:07:39 +0000

    body parsing start

--- server.js
+++ server.js
@@ -4,7 +4,18 @@ const express = require('express'),
 
 
 var app = express();
+var bodyParser = require('body-parser');
 
+//app.use(bodyParser());
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+//app.use(bodyParser.json());
+
+app.post("/testPost", function (req, res) {
+    console.log(req.body)
+	
+});
 
 app.get('/testResponse', function (req, res) {
    res.send('Hello World');
