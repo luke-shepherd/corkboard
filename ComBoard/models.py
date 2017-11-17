8ae282b9daa5f3c70924c037ed68bf97ba039113
Luke Shepherd <lukeshep@gmail.com>
2017-11-16 18:37:05 -0800

    db field update

--- ComBoard/models.py
+++ ComBoard/models.py
@@ -10,7 +10,7 @@ class Board(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField(auto_now_add=True)
     edit_date = models.DateTimeField(auto_now=True)
-    location = models.PointField()
+    location = models.GeometryField()
     creator = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 
 
