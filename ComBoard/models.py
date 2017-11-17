c271fecfb130bf0a4bf345bf8bb4290ea7a9d2c2
Samuel Levin <samueljlevin@gmail.com>
2017-11-16 19:11:23 -0800

    changed mix indent

--- ComBoard/models.py
+++ ComBoard/models.py
@@ -1,12 +1,13 @@
-#models.py
+# models.py
 
-#DATABASE Models 
+# DATABASE Models
 
 from django.contrib.gis.db import models
 
+
 class Board(models.Model):
-	public_viewable = models.BooleanField(default=False)
-	viewable_radius = models.IntegerField()
+    public_viewable = models.BooleanField(default=False)
+    viewable_radius = models.IntegerField()
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField(auto_now_add=True)
     edit_date = models.DateTimeField(auto_now=True)
@@ -15,9 +16,6 @@ class Board(models.Model):
 
 
 class BoardPost(models.Model):
-	parent_board = models.ForeignKey(Board, on_delete=models.CASCADE)
-	post_title = models.CharField(max_length=200)
-	post_body = models.CharField(max_length=4000)
-
-
-
+    parent_board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    post_title = models.CharField(max_length=200)
+    post_body = models.CharField(max_length=4000)
