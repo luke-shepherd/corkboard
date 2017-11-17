8a57ca3ae4d11126d8fa65c777378f921468041e
Samuel Levin <samueljlevin@gmail.com>
2017-11-17 15:54:08 -0800

    created app

--- ComBoard/settings.py.ex
+++ ComBoard/settings.py.ex
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
-    'ComBoard',
+    'rest_framework',
+    'comapi',
 ]
 
 MIDDLEWARE = [
