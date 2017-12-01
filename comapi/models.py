214ac3cc2ffe7088c1ed96b8e17977b3cfe163b4
Samuel Levin <samueljlevin@gmail.com>
2017-11-30 16:53:09 -0800

    models change

--- comapi/models.py
+++ comapi/models.py
@@ -4,12 +4,10 @@
 
 from django.contrib.gis.db import models
 from django.contrib.auth.models import User
-
-from django.conf import settings
 from django.contrib.auth import get_user_model
 
 
-#For Board/BoardPost on_delete. Deleted Users will not affect existing Boards.
+# For Board/BoardPost on_delete. Deleted Users will not affect existing Boards.
 def get_sentinel_user():
     return get_user_model().objects.get_or_create(username='deleted')[0]
 
@@ -21,7 +19,7 @@ class Board(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
     edit_date = models.DateTimeField(auto_now=True)
     location = models.GeometryField()
-    creator = models.ForeignKey('auth.User', on_delete=models.SET(get_sentinel_user))
+    creator = models.ForeignKey(User, on_delete=models.SET(get_sentinel_user))
 
 
 class BoardUser(models.Model):
@@ -32,7 +30,7 @@ class BoardUser(models.Model):
 
 class BoardPost(models.Model):
     parent_board = models.ForeignKey(Board, on_delete=models.CASCADE)
-    post_creator = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    post_creator = models.ForeignKey(User, on_delete=models.CASCADE,
+                                     default="", blank=False)
     post_title = models.CharField(max_length=200)
     post_body = models.CharField(max_length=4000)
-
